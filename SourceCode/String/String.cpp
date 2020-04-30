7778db96807071d169959a3ffb83b3bb05fa9b1f
Ankit Anvesh <ankit192812@rediffmail.com>
2020-04-30 14:20:21 +0530

    Update String.cpp
    Adding To Lower functionality in String.cpp
    Added function toLower(char [])

--- SourceCode/String/String.cpp
+++ SourceCode/String/String.cpp
@@ -6,6 +6,9 @@ void greet(char s[]) {
 void toupper(char s[]){
  printf("making char upper");
 }
+void toupper(char s[]){
+ printf("making char lower");
+}
 
 int main(){
  char gre[]="hello world";
