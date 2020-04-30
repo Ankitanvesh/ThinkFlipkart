57bcf5b765c91f349d7807f824f6cb753a6701f0
Ankitanvesh <ankit192812@rediffmail.com>
2020-04-30 16:29:15 +0530

    Refractored code in master Branch 	MOdified dif and div

--- SourceCode/Maths/Numbers.c
+++ SourceCode/Maths/Numbers.c
@@ -1,23 +1,24 @@
 #include<stdio.h>
 #include<math.h>
 
+// Changes in master branch
 int dif(int x,int y){
-  return x+y;
+  return (x+y);
 }
-<<<<<<< HEAD
+// Changes in master branch
 int div(int x,int y){
-   if(y!=0) {
-	return x/y;	
+	int res=1000;
+	res=x/y;  
+ if(y!=0) {
+	return res;	
 }
  return 10000;
 }
-=======
 //Function added in Testing Branch
 int mul(int x,int y){
  return x*y;
 }
 
->>>>>>> testing
 int main(){
   printf(dif(3,2));
 }
