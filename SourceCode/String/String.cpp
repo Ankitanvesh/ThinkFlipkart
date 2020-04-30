9a32149a7116ec23533c746b0055bcc0dbbce663
Ankitanvesh <ankit192812@rediffmail.com>
2020-04-30 15:14:01 +0530

    Change made to String.cpp by 	Adding title case functionality

--- SourceCode/String/String.cpp
+++ SourceCode/String/String.cpp
@@ -9,7 +9,9 @@ void toupper(char s[]){
 void toupper(char s[]){
  printf("making char lower");
 }
-
+void totalcase(char s[]){
+printf("Made something");
+}
 int main(){
  char gre[]="hello world";
 greet(gre);
